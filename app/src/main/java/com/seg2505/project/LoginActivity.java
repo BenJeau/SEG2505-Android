96dce43d624b6b5987ca0b407bd46eb220d136f2
Diedrick Ng <dngen049@uottawa.ca>
2018-10-24 15:31:09 -0400

    Modified activity-login

--- app/src/main/java/com/seg2505/project/LoginActivity.java
+++ app/src/main/java/com/seg2505/project/LoginActivity.java
@@ -2,12 +2,15 @@ package com.seg2505.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void setContent(View view){
+        setContentView(R.layout.activity_signup);
     }
 }
--- app/src/main/java/com/seg2505/project/SignupActivity.java
+++ app/src/main/java/com/seg2505/project/SignupActivity.java
@@ -10,6 +10,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -19,22 +21,55 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-    }
+        Button a  = (Button) findViewById(R.id.createUserButton);
 
-    databaseService = FirebaseDatabase.getInstance().getReference("products");
+        a.setOnClickListener(new View.OnClickListener() {
+            Person ac;
+            @Override
+            public void onClick(View view) {
+                EditText username = (EditText) findViewById(R.id.usernameEditText);
+                EditText psswrd = (EditText) findViewById(R.id.passwordEditText);
+                Spinner role = (Spinner)  findViewById(R.id.spinner);
 
-    public void CreateUser(View view){
-        EditText username = (EditText) findViewById(R.id.usernameEditText);
-        EditText psswrd = (EditText) findViewById(R.id.passwordEditText);
-        Spinner role = (Spinner)  findViewById(R.id.spinner);
 
 
-        if (role.getSelectedItem() == "Admin"){
-            Admin a = new Admin(username.getText().toString(), psswrd.getText().toString());
-        } else if(role.getSelectedItem() == "Service provider"){
-            Fournisseur f = new Fournisseur(username.getText().toString(), psswrd.getText().toString());
-        }else if(role.getSelectedItem() == "Proprietaire"){
-            Proprietaire p = new Proprietaire(username.getText().toString(), psswrd.getText().toString());
+                if (role.getSelectedItem() == "Admin"){
+                    ac = new Admin(username.getText().toString(), psswrd.getText().toString());
+                } else if(role.getSelectedItem() == "Service provider"){
+                    ac = new Fournisseur(username.getText().toString(), psswrd.getText().toString());
+                }else if(role.getSelectedItem() == "Proprietaire"){
+                    ac = new Proprietaire(username.getText().toString(), psswrd.getText().toString());
+                }
+                System.out.println(role.getSelectedItem());
+                TextView txt = (TextView) findViewById(R.id.textView);
+                txt.setText("jjjjj");
+                setContentView(R.layout.activity_login);
+            }
+        });
+
         }
-    }
+
+
+
+    //databaseService = FirebaseDatabase.getInstance().getReference("products");
+
+//    public void CreateUser(){
+//        EditText username = (EditText) findViewById(R.id.usernameEditText);
+//        EditText psswrd = (EditText) findViewById(R.id.passwordEditText);
+//        Spinner role = (Spinner)  findViewById(R.id.spinner);
+//
+//
+//
+//        if (role.getSelectedItem() == "Admin"){
+//             a = new Admin(username.getText().toString(), psswrd.getText().toString());
+//        } else if(role.getSelectedItem() == "Service provider"){
+//             a = new Fournisseur(username.getText().toString(), psswrd.getText().toString());
+//        }else if(role.getSelectedItem() == "Proprietaire"){
+//             a = new Proprietaire(username.getText().toString(), psswrd.getText().toString());
+//        }
+//        System.out.println(role.getSelectedItem());
+//        TextView txt = (TextView) findViewById(R.id.textView);
+//        txt.setText("jjjjj");
+//        setContentView(R.layout.activity_login);
+//    }
 }
