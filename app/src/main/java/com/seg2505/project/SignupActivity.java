08f7c4f86553e0c98d7437fbd8656bb471a6c3b4
Aymen <amprodz15@gmail.com>
2018-10-25 15:27:07 -0400

    added Admin login and sign up

--- app/src/main/java/com/seg2505/project/SignupActivity.java
+++ app/src/main/java/com/seg2505/project/SignupActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -25,12 +26,13 @@ import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity {
 
-    DatabaseReference databaseReference;
+    DatabaseReference databaseReference,adminReference;
     Button a;
     Spinner role;
     EditText edtUsername;
     EditText edtPassword;
     FirebaseDatabase database;
+    boolean adminExists = false;
     boolean exists = false;
 
 
@@ -49,6 +51,8 @@ public class SignupActivity extends AppCompatActivity {
 
 
         databaseReference = database.getReference("users");
+        adminReference = database.getReference("Admin");
+
 
 
         a.setOnClickListener(new View.OnClickListener() {
@@ -56,78 +60,111 @@ public class SignupActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String username = edtUsername.getText().toString();
                 String password = edtPassword.getText().toString();
-                if(TextUtils.isEmpty(username)) {
-                    edtUsername.setError("User field cannot be empty.");
-                }
-                if(TextUtils.isEmpty(password)) {
-                    edtPassword.setError("Password field cannot be empty.");
-                }
-                if(username.length()<3 && !TextUtils.isEmpty(username)) {
+                if(username.equals("Admin") && password.equals("Admin")){
+                    adminReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            long adminCount = dataSnapshot.getChildrenCount();
 
-                    edtUsername.setError("Username must be 3 characters long.");
-                }
-                if (!isValidPassword(password) &&!TextUtils.isEmpty(password) ){
-                    edtPassword.setError("Password must be a minimum of 4 characters and have at least one capital letter and one number.");
-                }
+                            if(adminCount==(long)1) {
+                                Log.i("tagged", adminCount+ "");
+                                adminExists=true;
+                                Toast.makeText(getApplicationContext(), "Admin already exists", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                            if (!adminExists){
+                                adminReference.push().setValue(new Admin(edtUsername.getText().toString(),edtPassword.getText().toString()));
+                                Toast.makeText(getApplicationContext(), "Admin successfully created", Toast.LENGTH_SHORT).show();
 
-                databaseReference.addListenerForSingleValueEvent(new ValueEventListener(){
-
-
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        Person user = null;
-                        for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
-                            user = postSnapshot.getValue(Person.class);
-
-                            if (edtUsername.getText().toString().equals(user.getEmail())){
-                                exists = true;
-
-                                break;
-
+                                Intent intent = new Intent(SignupActivity.this,LoginActivity.class);
+                                startActivity(intent);
                             }
                         }
-                        if(exists) {
-
-                            edtUsername.setError("Username already exist");
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-
-
-                Person ac = null;
-
-                if (username.length()>2 && isValidPassword(password) && !exists ){
-                    final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this, R.style.AppTheme_Blue_Dialog);
-                    progressDialog.setIndeterminate(true);
-                    progressDialog.setCancelable(false);
-                    progressDialog.setMessage("Creating account...");
-                    progressDialog.show();
-                    final Handler handler = new Handler();
-                    final Runnable r = new Runnable(){
 
                         @Override
-                        public void run() {
-                            progressDialog.dismiss();
-                            Toast.makeText(getApplicationContext(), "Account successfully created", Toast.LENGTH_SHORT).show();
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                            Intent intent = new Intent(SignupActivity.this,LoginActivity.class);
-                            startActivity(intent);
                         }
-                    };
-                    if (role.getSelectedItem().equals("Provider")) {
-                        ac = new Provider(username, password);
-                    } else if (role.getSelectedItem().equals("User")) {
-                        ac = new Owner(username, password);
-                    }
-                    databaseReference.push().setValue(ac);
-                    handler.postDelayed(r,1000);
+                    });
 
+
+                }else {
+                    if(TextUtils.isEmpty(username)) {
+                        edtUsername.setError("User field cannot be empty.");
+                    }
+                    if(TextUtils.isEmpty(password)) {
+                        edtPassword.setError("Password field cannot be empty.");
+                    }
+                    if(username.length()<3 && !TextUtils.isEmpty(username)) {
+
+                        edtUsername.setError("Username must be 3 characters long.");
+                    }
+                    if (!isValidPassword(password) &&!TextUtils.isEmpty(password) ){
+                        edtPassword.setError("Password must be a minimum of 4 characters and have at least one capital letter and one number.");
+                    }
+
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener(){
+
+
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            Person user = null;
+                            for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                                user = postSnapshot.getValue(Person.class);
+
+                                if (edtUsername.getText().toString().equals(user.getEmail())){
+                                    exists = true;
+
+                                    break;
+
+                                }
+                            }
+                            if(exists) {
+
+                                edtUsername.setError("Username already exist");
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+
+
+                    Person ac = null;
+
+                    if (username.length()>2 && isValidPassword(password) && !exists ){
+                        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this, R.style.AppTheme_Blue_Dialog);
+                        progressDialog.setIndeterminate(true);
+                        progressDialog.setCancelable(false);
+                        progressDialog.setMessage("Creating account...");
+                        progressDialog.show();
+                        final Handler handler = new Handler();
+                        final Runnable r = new Runnable(){
+
+                            @Override
+                            public void run() {
+                                progressDialog.dismiss();
+                                Toast.makeText(getApplicationContext(), "Account successfully created", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(SignupActivity.this,LoginActivity.class);
+                                startActivity(intent);
+                            }
+                        };
+                        if (role.getSelectedItem().equals("Provider")) {
+                            ac = new Provider(username, password);
+                        } else if (role.getSelectedItem().equals("Owner")) {
+                            ac = new Owner(username, password);
+                        }
+                        databaseReference.push().setValue(ac);
+                        handler.postDelayed(r,1000);
+
+                    }
                 }
+
 
 
 
