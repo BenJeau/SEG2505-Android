b2177207388d88980f38d7476184e84a613e5003
vergenieh <vergenie.h@hotmail.com>
2018-11-08 17:01:16 -0500

    Added test cases for username and password during login.

---
+++ app/src/test/java/com/seg2505/project/LoginUnitTest.java
@@ -0,0 +1,48 @@
+package com.seg2505.project;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class LoginUnitTest {
+    @Test
+    public void checkWrongUserPassword() {
+        Boolean valid = LoginActivity.isValidPassword("hi");
+        assertEquals("Check if the password is invalid.", false, valid);
+    }
+
+    @Test
+    public void checkRightUserPassword() {
+        Boolean valid = LoginActivity.isValidPassword("BooMessi100%");
+        assertEquals("Check if the password is valid.", true, valid);
+    }
+
+    @Test
+    public void checkEmptyUserPassword() {
+        Boolean valid = LoginActivity.isValidPassword("");
+        assertEquals("Check if the password is invalid.", false, valid);
+
+    }
+
+    @Test
+    public void checkRightUser() {
+        Boolean valid = LoginActivity.isValidUsername("vergenieh");
+        assertEquals("Check if username is valid.", true, valid);
+    }
+
+    @Test
+    public void checkWrongUser() {
+        Boolean valid = LoginActivity.isValidUsername("");
+        assertEquals("Check if username is invalid.", false, valid);
+    }
+
+    @Test
+    public void checkRightAdmin(){
+        Boolean valid = LoginActivity.isValidAdministrator("admin", "admin");
+        assertEquals("Check if administrator is valid.", true, valid);
+    }
+
+    @Test
+    public void checkWrongAdmin() {
+        Boolean valid = LoginActivity.isValidAdministrator("YakMessi", "BooMessi100%");
+        assertEquals("Check if administrator is invalid.", false, valid);
+    }
+}
