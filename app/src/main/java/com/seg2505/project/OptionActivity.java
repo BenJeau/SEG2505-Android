bfc22f19dcc112e9aff1c2bf3921e125faf33aa7
Diedrick Ng <dngen049@uottawa.ca>
2018-11-08 14:12:10 -0500

    Added a new Activity

---
+++ app/src/main/java/com/seg2505/project/OptionActivity.java
@@ -0,0 +1,37 @@
+package com.seg2505.project;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class OptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option);
+
+        Button ViewUser = (Button) findViewById(R.id.BtnViewUser);
+        Button ViewService = (Button) findViewById(R.id.BtnViewServices);
+
+        ViewService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OptionActivity.this,AdminActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        ViewService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OptionActivity.this,OptionActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
