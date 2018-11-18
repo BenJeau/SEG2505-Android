60e10a9c286df939c1b1392107b7e4bc12225e25
Benoît Jeaurond <bensonj@hotmail.ca>
2018-11-18 15:59:17 -0500

    Created ProviderInfo activity and fixed the bugs

---
+++ app/src/main/java/com/seg2505/project/activities/ProviderInfoActivity.java
@@ -0,0 +1,15 @@
+package com.seg2505.project.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.seg2505.project.R;
+
+public class ProviderInfoActivity  extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_provider_info);
+        }
+
+}
